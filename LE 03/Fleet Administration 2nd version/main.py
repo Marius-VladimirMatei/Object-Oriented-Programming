26e5598d2f6a2bdaf9511c0c8fbb6f67f9777b05
Marius-Vladimir Matei <052583@edu.szf.at>
2025-02-04 15:49:31 +0100

    main with examples

--- LE 03/Fleet Administration 2nd version/main.py
+++ LE 03/Fleet Administration 2nd version/main.py
@@ -5,32 +5,45 @@ from car import Car
 from cargo_truck import Cargo_truck
 from motorcycle import Motorcycle
 from bicycle import Bicycle
+from vehicle_data_base import Vehicle_Data_Base
 
 
-car_1 = Car(
-    1,
-    "G123AB",
-    "Ford",
-    "Mondeo",
-    "2024",
-    5000,
-    "Petrol",
-    55,
-    5
-)
 
-cargo_truck_1 = Cargo_truck(
-    2,
-    "CA123NA",
-    "Mercedes",
-    "Actros",
-    2022,
-    525000,
-    "Diesel",
-    450,
-    2500,
-    12000
-)
+
+if __name__ == "__main__":
+    data_base = Vehicle_Data_Base("vehicles.txt")
+
+    car_1 = Car(
+                1,
+                "G123AB",
+                "Ford",
+                "Mondeo",
+                "2024",
+                5000,
+                "Petrol",
+                55,
+                5
+                )
+
+    cargo_truck_1 = Cargo_truck(
+        2,
+        "CA123NA",
+        "Mercedes",
+        "Actros",
+        2022,
+        525000,
+        "Diesel",
+        450,
+        2500,
+        12000
+    )
+
+    data_base.add_vehicle(car_1)
+    data_base.add_vehicle(cargo_truck_1)
+
+    data_base.list_vehicles()
+    data_base.save_vehicle()
+
 
 
 car_1.refuel(55)
@@ -38,3 +51,14 @@ car_1.refuel(55)
 cargo_truck_1.load(7950)
 
 cargo_truck_1.unload(950)
+
+
+
+
+
+
+
+
+
+
+
