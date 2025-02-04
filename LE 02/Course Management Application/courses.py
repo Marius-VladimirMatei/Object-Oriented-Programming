c9c8a8c41e72f8568772f8628059cbe361c297bc
Marius-VladimirMatei <052583@edu.szf.at>
2025-02-04 12:59:25 +0100

    Delete LE 02/Course Management Application directory

--- LE 02/Course Management Application/courses.py
+++
@@ -1,9 +0,0 @@
-class Course:
-    def __init__(self, course_name, course_id, start_date, end_date):
-        self.course_name = course_name
-        self.course_id = course_id
-        self.start_date = start_date
-        self.end_date = end_date
-
-    def __str__(self):
-        return f"Course Name: {self.course_name}, Course ID: {self.course_id}, Start Date: {self.start_date}, End Date: {self.end_date}"
